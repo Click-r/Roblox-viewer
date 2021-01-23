080ece8cfe808fbd14d0ddf1069792ef5a6c7937
Click-r <72707653+Click-r@users.noreply.github.com>
2021-01-23 17:56:23 +0100

    implement threadpool
    Implemented a ThreadPoolExecutor to shorten the code and improve performance

--- src/main/getInfo.java
+++ src/main/getInfo.java
@@ -12,62 +12,66 @@ import java.net.*;
 
 import java.util.Stack;
 
+import java.util.concurrent.*;
+
 public class getInfo {
 
     @SuppressWarnings("unchecked")
 
     public static HashMap<String, String> searchByUsername(String username){
-        Stack<Long> id = new Stack<Long>();
         final Charset UTF_8 = StandardCharsets.UTF_8;
+        ExecutorService exec = Executors.newSingleThreadExecutor();
 
-        Thread toRun = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    URL apiEndpoint = new URL("https://users.roblox.com/v1/usernames/users");
+        Future<Long> id = exec.submit(() -> {
+            try {
+                URL apiEndpoint = new URL("https://users.roblox.com/v1/usernames/users");
 
-                    URLConnection apiConnect = apiEndpoint.openConnection();
-                    HttpURLConnection httpCon = (HttpURLConnection)apiConnect;
+                URLConnection apiConnect = apiEndpoint.openConnection();
+                HttpURLConnection httpCon = (HttpURLConnection)apiConnect;
 
-                    byte[] out = ("{\"usernames\":[\"" + username +"\"], \"excludeBannedUsers\":false}").getBytes(UTF_8);
-                    int len = out.length;
+                byte[] out = ("{\"usernames\":[\"" + username +"\"], \"excludeBannedUsers\":false}").getBytes(UTF_8);
+                int len = out.length;
 
-                    httpCon.setRequestMethod("POST");
-                    httpCon.setDoOutput(true);
-                    httpCon.setFixedLengthStreamingMode(len);
-                    httpCon.setRequestProperty("Content-type", "application/json; charset=UTF-8");
-                    httpCon.connect();
+                httpCon.setRequestMethod("POST");
+                httpCon.setDoOutput(true);
+                httpCon.setFixedLengthStreamingMode(len);
+                httpCon.setRequestProperty("Content-type", "application/json; charset=UTF-8");
+                httpCon.connect();
 
-                    try (OutputStream os = httpCon.getOutputStream()) {
-                        os.write(out);
-                    }
-
-                    InputStream response = httpCon.getInputStream();
-                    String txtResponse = "NaN";
-
-                    try (Scanner scanner = new Scanner(response)){
-                        txtResponse = scanner.useDelimiter("\\A").next();
-                    }
-
-                    id.push(Long.valueOf((String)((HashMap<String, Object>)JSONtoHashtable.toHashtable(txtResponse).get("data")).get("id")));
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                try (OutputStream os = httpCon.getOutputStream()) {
+                    os.write(out);
                 }
-            }
-        };
 
-        toRun.run();
+                InputStream response = httpCon.getInputStream();
+                String txtResponse = "NaN";
+
+                try (Scanner scanner = new Scanner(response)){
+                    txtResponse = scanner.useDelimiter("\\A").next();
+                }
+
+                return Long.valueOf((String)((HashMap<String, Object>)JSONtoHashtable.toHashtable(txtResponse).get("data")).get("id"));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            return 1L;
+        });
 
         HashMap<String, String> requested = new HashMap<String, String>();
 
-        if (id.size() == 1) {
-            long Id = id.pop();
-            try {
-                requested = getInformation(Id);
-            } catch (SocketTimeoutException err) {
-                System.out.println("Timed out");
-            }
+        long Id = 1L;
+
+        try {
+            Id = id.get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            return requested;
+        }
+
+        try {
+            requested = getInformation(Id);
+        } catch (SocketTimeoutException err) {
+            System.out.println("Timed out");
         }
 
         return requested;
@@ -91,91 +95,63 @@ public class getInfo {
         boolean properlyParsed = true;
 
         // Start of multi-threaded data retrieval
-        Stack<Runnable> tasks = new Stack<Runnable>();
-        Stack<Thread> usable = new Stack<Thread>();
         Stack<Integer> buffer = new Stack<Integer>();
 
+        ThreadPoolExecutor retrieve = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
+
         for (final String domain : apiDomains){
-
-            Runnable task = new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        URL api = new URL(domain);
-                        URLConnection apiConnection = api.openConnection();
-                        apiConnection.setConnectTimeout(5000);
-                        apiConnection.connect();
-                        InputStream response = apiConnection.getInputStream();
-                        String textResponse = "NaN";
-        
-                        try (Scanner scanner = new Scanner(response)) {
-                            String responseBody = scanner.useDelimiter("\\A").next();
-                            textResponse = responseBody;
-                        }
-        
-                        HashMap<String,Object> subData = JSONtoHashtable.toHashtable(textResponse);
-
-                        subData.forEach( 
-                            (key,val) ->
-                                {
-                                    String name = key;
-                                    if (subData.size() == 1){
-                                        String[] urlName = domain.split("/");
-                                        name = urlName[urlName.length - 2];
-                                    }
-                                    data.put(name, (String)val);
-                                }
-                        );
-                        
-                        buffer.push(1); // fill buffer upon thread completion
-                        usable.push(new Thread()); // push another thread back onto stack to perform next activity (if any)
-        
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {}
-                }
-            };
-
-            tasks.push(task);
-        }
-
-        final int proc = Runtime.getRuntime().availableProcessors();
-        int maxUtilize = (proc > 5) ? 5 : proc; // see how much threads are able to be utilised
-
-        for (int i = 0; i < maxUtilize; i++) { // populate stack with usable threads
-            Thread completeTask = new Thread();
-            usable.push(completeTask);
-        }
-
-        long started = System.currentTimeMillis();
-        Thread executeOn = new Thread();
-
-        while (buffer.size() != 5) { // start running threads
-            if (tasks.size() != 0 && !usable.isEmpty()) {
-                Runnable current = tasks.pop();
-                usable.pop();
-
-                executeOn = new Thread() {
-                    public void run() {
-                        current.run();
+            retrieve.submit(() -> {
+                try {
+                    URL api = new URL(domain);
+                    URLConnection apiConnection = api.openConnection();
+                    apiConnection.setConnectTimeout(5000);
+                    apiConnection.connect();
+                    InputStream response = apiConnection.getInputStream();
+                    String textResponse = "NaN";
+            
+                    try (Scanner scanner = new Scanner(response)) {
+                        String responseBody = scanner.useDelimiter("\\A").next();
+                        textResponse = responseBody;
                     }
-                };
+            
+                    HashMap<String,Object> subData = JSONtoHashtable.toHashtable(textResponse);
 
-                executeOn.start(); // execute the runnable
-            }
-
-            if (System.currentTimeMillis() - started >= 1000){
-                properlyParsed = false;
-                executeOn.interrupt();
-                System.out.println("interrupted");
-                // if the thread hangs for some reason then interrupt execution so the program doesn't get stuck
-                break;
-            }
-                
+                    subData.forEach( 
+                        (key,val) ->
+                            {
+                                String name = key;
+                                if (subData.size() == 1){
+                                    String[] urlName = domain.split("/");
+                                    name = urlName[urlName.length - 2];
+                                }
+                                data.put(name, (String)val);
+                            }
+                        );
+                            
+                    buffer.push(1); // fill buffer upon thread completion
+            
+                    } catch (IOException e) {
+                        int remainingBuffer = 5 - buffer.size();
+                        for (int i = 0; i < remainingBuffer; i++)
+                            buffer.push(1);
+                        return;
+                    }
+                }
+            );
         }
+
+        while (buffer.size() < 5) {
+            try {
+                Thread.sleep(2L);
+            } catch (InterruptedException inter) {}
+        }
+
+        retrieve.shutdownNow();
         //End of multi-threaded data retrieval
 
-        if (properlyParsed){
+        properlyParsed &= data.size() == 10;
+
+        if (properlyParsed) {
             String creationDate = data.get("created").split("T")[0];
             
             data.replace("created", creationDate.substring(1,creationDate.length()));
