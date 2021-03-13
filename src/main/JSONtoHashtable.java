19ff2c9331a952b668af23d29b67cd9b50c5fda0
Click-r <72707653+Click-r@users.noreply.github.com>
2021-03-13 10:10:19 +0100

    make code cleaner
    Finally got around to cleaning up the JSONtoHashtable file. The regex patterns still look as if I trampled all over my keyboard but that's unavoidable.

--- src/main/JSONtoHashtable.java
+++ src/main/JSONtoHashtable.java
@@ -11,57 +11,44 @@ public class JSONtoHashtable {
         ArrayList<Object> values = new ArrayList<Object>();
     
         HashMap<String, Object> data = new HashMap<String, Object>();
-        final String patternUsing = "\"(\\.|[^\"])+[^\\\\]\"(?=\\s*:([\"0-9]|(false|true|null)))"; // slightly altered version of https://regexr.com/56m28
-        final String arrayPattern = "(?=[^\"])\\[.+?(\\])(?=,|})"; // https://regexr.com/5e4v3 (created by me)
+        final String patternUsing = "\"((\\.|[^\"])+[^\\\\])\"(?=\\s*:([\"0-9]|(false|true|null)))"; // slightly altered version of https://regexr.com/56m28
+        final String arrayPattern = "\"([a-z_-]+?)\":(?=[^\"])(\\[.+?(\\])(?=,|}))"; // https://regexr.com/5e4v3 (created by me)
 
         Pattern arrayKey = Pattern.compile(arrayPattern);
         Matcher array = arrayKey.matcher(JSONString);
 
         while (array.find()){
             int startInd = array.start();
-            if (! JSONString.substring(startInd-3,startInd).equals("\\\":")){
-                String arrayVal = array.group();
-                String foundKey = "";
 
-                byte temp = 0;
-                for (int c = startInd; c <= startInd; c--){
-                    if (JSONString.charAt(c) == '\"' && temp != 2){
-                        temp++;
-                        if (temp == 2){
-                            foundKey = JSONString.substring(c,startInd-1);
-                            JSONString = JSONString.substring(0,c-1) + JSONString.substring(array.end()+1,JSONString.length());
-                            break;
-                        }
-                    }
-                }
+            String key = array.group(1);
+            String arr = array.group(2);
+            arr = arr.substring(1,arr.length());
 
-                HashMap<String,Object> returned = toHashtable(arrayVal.substring(1,arrayVal.length()));
-                keys.add(foundKey.substring(1,foundKey.length()-1));
-                values.add(returned);
-            }
+            HashMap<String, Object> parsed = toHashtable(arr);
+
+            JSONString = JSONString.substring(0, startInd-1) + JSONString.substring(array.end() + 1, JSONString.length()); // delete array from string
+
+            keys.add(key);
+            values.add(parsed);
         } // handles the key-value pairs associated with arrays in the data set
 
         Pattern key = Pattern.compile(patternUsing);
         Matcher message = key.matcher(JSONString);
 
-        while (message.find()){
-            String find = message.group();
-            keys.add(find.substring(1,find.length()-1));
-        } // handles the rest of the non-array associated keys in the data set
+        while (message.find())
+            keys.add(message.group(1)); // handles the rest of the non-array associated keys in the data set
 
         Scanner scanner = new Scanner(JSONString);
         scanner.useDelimiter(patternUsing);
 
         while (scanner.hasNext()){
             String val = scanner.next();
-            if ( !(val.equals("}")|val.equals("{")) ){
 
-                val = val.substring(1,val.length()-1);
-
-                val = (val.length() == 0) ? "not set" : val;
-
-                val = val.replaceAll("\\\\\"", "\"");
-
+            if (!(val.equals("}") || val.equals("{"))){
+                val = val
+                    .substring(1,val.length()-1) //remove quotes
+                    .replaceAll("\\\\\"", "\""); //replace escaped characters
+                val = val.isEmpty() ? "not set" : val;  //check if empty
                 values.add(val);
             }
         } // handles the rest of the non-array associated values in the data set
@@ -74,4 +61,3 @@ public class JSONtoHashtable {
         
     }
 }
-// TODO: make this more readable
